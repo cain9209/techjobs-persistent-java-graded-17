7e1b99c1db41e3c560bfd22ae435a9b4946e7e67
Zachary Cain <zacharylcain@gmail.com>
2024-06-04 15:28:27 -0500

    passed 12/18 test for test task two

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location is Mandatory")
+    @Size(max = 100,message = "Location must be less than 100 characters")
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
