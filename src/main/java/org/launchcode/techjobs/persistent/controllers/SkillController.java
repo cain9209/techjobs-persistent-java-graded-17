0586cbb70c80b7808943719eb3836542d5e57bb3
Zachary Cain <zacharylcain@gmail.com>
2024-06-06 18:41:17 -0500

    After 24 test to increase to 27

--- src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -1,4 +1,63 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("skills")
 public class SkillController {
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("skills", skillRepository.findAll());
+        return "skills/index";
+    }
+
+
+    // This test is still failing //
+    @GetMapping("add")
+    public String displayAddEmployerForm(Model model) {
+        model.addAttribute(new Employer());
+        return "employers/add";
+    }
+
+    @PostMapping("add")
+    public String processAddEmployerForm(@ModelAttribute @Valid Employer newEmployer,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "employers/add";
+        }
+
+        skillRepository.save(newEmployer);
+        return "redirect:";
+    }
+
+    @GetMapping("view/{employerId}")
+    public String displayViewEmployer(Model model, @PathVariable int employerId) {
+
+        Optional<Employer> optEmployer = skillRepository.findById(employerId);
+        if (optEmployer.isPresent()) {
+            Employer employer = (Employer) optEmployer.get();
+            model.addAttribute("employer", employer);
+            return "employers/view";
+        } else {
+            return "redirect:../";
+        }
+
+    }
+
+
 }
