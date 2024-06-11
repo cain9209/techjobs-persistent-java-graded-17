0af00f3de64d504371727de7f4d561a3442a0496
Zachary Cain <zacharylcain@gmail.com>
2024-06-11 14:43:16 -0500

    Task 3 one test to pass before 4

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,12 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
 public class Employer extends AbstractEntity {
+
+    @OneToMany
+    @JoinColumn(name = "employer_id", nullable = false)
+    private List<Job> jobs = new ArrayList<>();
+
+
 
     @NotBlank(message = "Location is Mandatory")
     @Size(max = 100,message = "Location must be less than 100 characters")
